6b775a2208b95cb701b6faa5e7c0fbfa90b270be
Morgan <mrgnhnt96@gmail.com>
2022-10-28 13:47:19 -0500

    feat: await cache init

--- repo/__brick__/{{{path}}}/{{#snakeCase}}{{{name}}}{{/snakeCase}}/{{#snakeCase}}{{{name}}}_cache{{/snakeCase}}.dart
+++ repo/__brick__/{{{path}}}/{{#snakeCase}}{{{name}}}{{/snakeCase}}/{{#snakeCase}}{{{name}}}_cache{{/snakeCase}}.dart
@@ -7,32 +7,44 @@ class {{#pascalCase}}{{{name}}}{{/pascalCase}}Cache extends HiveCache<Map> imple
 
   @override
   Future<RequestResult<List<{{#pascalCase}}{{{model}}}{{/pascalCase}}>>> all() async {
+    await cacheInit();
+
     throw UnimplementedError('all() has not been implemented.');
   }
 
   @override
   Future<RequestResult<{{#pascalCase}}{{{model}}}{{/pascalCase}}>> byId(String id) async {
+    await cacheInit();
+
     throw UnimplementedError('byId() has not been implemented.');
   }
 
   @override
   Future<RequestResult<void>> delete(String id) async {
+    await cacheInit();
+
     throw UnimplementedError('delete() has not been implemented.');
   }
 
   @override
   Future<RequestResult<void>> deleteAll() async {
+    await cacheInit();
+
     throw UnimplementedError('deleteAll() has not been implemented.');
   }
 
   @override
   Future<RequestResult<{{#pascalCase}}{{{model}}}{{/pascalCase}}>> save({{#pascalCase}}{{{model}}}{{/pascalCase}} {{#camelCase}}{{{model}}}{{/camelCase}}) async {
+    await cacheInit();
+
     throw UnimplementedError('save() has not been implemented.');
   }
 
   @override
   Future<RequestResult<{{#pascalCase}}{{{model}}}{{/pascalCase}}>> saveAll(
       List<{{#pascalCase}}{{{model}}}{{/pascalCase}}> {{#camelCase}}{{{model}}}{{/camelCase}}s) async {
+    await cacheInit();
+
     throw UnimplementedError('saveAll() has not been implemented.');
   }
 }
--- repo/oven/path/example/example_cache.dart
+++ repo/oven/path/example/example_cache.dart
@@ -7,32 +7,44 @@ class _NAME_pascalCache extends HiveCache<Map> implements I_NAME_pascalCache {
 
   @override
   Future<RequestResult<List<_MODEL_pascal>>> all() async {
+    await cacheInit();
+
     throw UnimplementedError('all() has not been implemented.');
   }
 
   @override
   Future<RequestResult<_MODEL_pascal>> byId(String id) async {
+    await cacheInit();
+
     throw UnimplementedError('byId() has not been implemented.');
   }
 
   @override
   Future<RequestResult<void>> delete(String id) async {
+    await cacheInit();
+
     throw UnimplementedError('delete() has not been implemented.');
   }
 
   @override
   Future<RequestResult<void>> deleteAll() async {
+    await cacheInit();
+
     throw UnimplementedError('deleteAll() has not been implemented.');
   }
 
   @override
   Future<RequestResult<_MODEL_pascal>> save(_MODEL_pascal _MODEL_camel) async {
+    await cacheInit();
+
     throw UnimplementedError('save() has not been implemented.');
   }
 
   @override
   Future<RequestResult<_MODEL_pascal>> saveAll(
       List<_MODEL_pascal> _MODEL_camels) async {
+    await cacheInit();
+
     throw UnimplementedError('saveAll() has not been implemented.');
   }
 }
