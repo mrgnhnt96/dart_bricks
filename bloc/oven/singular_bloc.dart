9c6f9549b9e3931c30e751d3b2483ec525fb8109
Morgan <mrgnhnt96@gmail.com>
2023-02-28 15:33:47 -0700

    style: organize members

--- bloc/oven/singular_bloc.dart
+++ bloc/oven/singular_bloc.dart
@@ -47,50 +47,6 @@ class _NAME_PASCALBloc
     await super.close();
   }
 
-  Future<void> _fetch(_Fetch event, _Emitter emit) async {
-    __NAME_CAMELId = event.id;
-
-    await hydrate(emit);
-
-    final result = await __NAME_CAMELsSource.byId(__NAME_CAMELId);
-
-    if (result.isError) {
-      emitError(emit, _Error(result.error.message));
-      return;
-    }
-
-    emit(_Ready(result.value));
-
-    final stream = await __NAME_CAMELsSource.watchById(__NAME_CAMELId);
-
-    await _listener?.cancel();
-
-    _listener = stream.listen((event) {
-      if (event.wasDeleted) {
-        add(const _SourceChanged(_Error('_NAME_SENTENCE was deleted')));
-        return;
-      }
-
-      final _NAME_CAMEL = event.value;
-
-      if (_NAME_CAMEL == null) {
-        return;
-      }
-
-      final newState = _Ready(_NAME_CAMEL);
-
-      if (newState == state) {
-        return;
-      }
-
-      add(_SourceChanged(newState));
-    });
-  }
-
-  FutureOr<void> _cacheChanged(_CacheChanged event, _Emitter emit) async {
-    emitNoCache(emit, event.state);
-  }
-
   @override
   Future<Stream<_NAME_PASCALState?>> listenForStorageChanges() async {
     final result = await __NAME_CAMELsCache.watchAll();
@@ -151,15 +107,8 @@ class _NAME_PASCALBloc
     return _Ready(result.value);
   }
 
-  FutureOr<void> _save(_Save event, _Emitter emit) async {
-    final result = await __NAME_CAMELsSource.update(event._NAME_CAMEL);
-
-    if (result.isError) {
-      emitError(emit, _Error(result.error.message));
-      return;
-    }
-
-    emit(_Ready(event._NAME_CAMEL));
+  FutureOr<void> _cacheChanged(_CacheChanged event, _Emitter emit) async {
+    emitNoCache(emit, event.state);
   }
 
   FutureOr<void> _create(_Create event, _Emitter emit) async {
@@ -173,12 +122,6 @@ class _NAME_PASCALBloc
     emit(_Ready(result.value));
   }
 
-  FutureOr<void> _sourceChanged(_SourceChanged event, _Emitter emit) async {
-    await stopListeningToChanges();
-
-    emit(event.state);
-  }
-
   FutureOr<void> _delete(_Delete event, _Emitter emit) async {
     await __NAME_CAMELsCache.delete(event.id);
 
@@ -190,5 +133,62 @@ class _NAME_PASCALBloc
     }
 
     emit(const _Deleted());
+  }
+
+  Future<void> _fetch(_Fetch event, _Emitter emit) async {
+    __NAME_CAMELId = event.id;
+
+    await hydrate(emit);
+
+    final result = await __NAME_CAMELsSource.byId(__NAME_CAMELId);
+
+    if (result.isError) {
+      emitError(emit, _Error(result.error.message));
+      return;
+    }
+
+    emit(_Ready(result.value));
+
+    final stream = await __NAME_CAMELsSource.watchById(__NAME_CAMELId);
+
+    await _listener?.cancel();
+
+    _listener = stream.listen((event) {
+      if (event.wasDeleted) {
+        add(const _SourceChanged(_Error('_NAME_SENTENCE was deleted')));
+        return;
+      }
+
+      final _NAME_CAMEL = event.value;
+
+      if (_NAME_CAMEL == null) {
+        return;
+      }
+
+      final newState = _Ready(_NAME_CAMEL);
+
+      if (newState == state) {
+        return;
+      }
+
+      add(_SourceChanged(newState));
+    });
+  }
+
+  FutureOr<void> _save(_Save event, _Emitter emit) async {
+    final result = await __NAME_CAMELsSource.update(event._NAME_CAMEL);
+
+    if (result.isError) {
+      emitError(emit, _Error(result.error.message));
+      return;
+    }
+
+    emit(_Ready(event._NAME_CAMEL));
+  }
+
+  FutureOr<void> _sourceChanged(_SourceChanged event, _Emitter emit) async {
+    await stopListeningToChanges();
+
+    emit(event.state);
   }
 }
