4604b08b00f846beb0e3c54b605f745fd57a3535
Morgan <mrgnhnt96@gmail.com>
2023-03-01 08:20:54 -0700

    feat: add firebase and dio specific source files

--- failure/__brick__/{{#snakeCase}}{{{name}}}{{/snakeCase}}s/{{#snakeCase}}{{{name}}}{{/snakeCase}}s.dart
+++
@@ -1,3 +0,0 @@
-export '{{#snakeCase}}{{{name}}}{{/snakeCase}}s_cache.dart';
-export '{{#snakeCase}}{{{name}}}{{/snakeCase}}s_repos.dart';
-export '{{#snakeCase}}{{{name}}}{{/snakeCase}}s_source.dart';
--- failure/oven/name/name.dart
+++
@@ -1,3 +0,0 @@
-export '_NAME_SNAKEs_cache.dart';
-export '_NAME_SNAKEs_repos.dart';
-export '_NAME_SNAKEs_source.dart';
