2de948ba1b3e0602f1ce056da3788c30d245eda2
Morgan <mrgnhnt96@gmail.com>
2023-03-22 18:00:00 -0500

    refactor: move repo interfaces to sub dir

---
+++ model/__brick__/{{#is_repository}}{{#snakeCase}}{{{name}}}{{/snakeCase}}{{/is_repository}}/{{#snakeCase}}{{{name}}}{{/snakeCase}}.dart
@@ -0,0 +1,2 @@
+export 'i_{{#snakeCase}}{{{name}}}{{/snakeCase}}s_cache.dart';
+export 'i_{{#snakeCase}}{{{name}}}{{/snakeCase}}s_source.dart';
---
+++ model/oven/repo/repo.dart
@@ -0,0 +1,2 @@
+export 'i__NAME_SNAKEs_cache.dart';
+export 'i__NAME_SNAKEs_source.dart';
