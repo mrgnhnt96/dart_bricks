51f394aaa9176a5b7ba6a6a29072fc1fa58300aa
Morgan <mrgnhnt96@gmail.com>
2023-03-22 17:56:31 -0500

    fix: add cache.dart to brick_oven

---
+++ model/__brick__/i_{{#snakeCase}}{{{name}}}{{/snakeCase}}_cache.dart
@@ -0,0 +1,20 @@
+import 'package:internal_domain/models/{{#snakeCase}}{{{name}}}{{/snakeCase}}.dart';
+import 'package:internal_domain/models/request_result.dart';
+
+abstract class I{{#pascalCase}}{{{name}}}{{/pascalCase}}sCache {
+  Future<RequestResult<List<{{#pascalCase}}{{{name}}}{{/pascalCase}}>>> all();
+
+  Future<RequestResult<{{#pascalCase}}{{{name}}}{{/pascalCase}}>> byId(String id);
+
+  Future<RequestResult<void>> delete(String id);
+
+  Future<RequestResult<void>> deleteAll([Set<String>? ids]);
+
+  Future<RequestResult<void>> save({{#pascalCase}}{{{name}}}{{/pascalCase}} {{#camelCase}}{{{name}}}{{/camelCase}});
+
+  Future<RequestResult<void>> saveAll(List<{{#pascalCase}}{{{name}}}{{/pascalCase}}> {{#camelCase}}{{{name}}}{{/camelCase}}s);
+
+  Future<Stream<StreamResult<{{#pascalCase}}{{{name}}}{{/pascalCase}}>>> watchAll();
+
+  Future<Stream<StreamResult<{{#pascalCase}}{{{name}}}{{/pascalCase}}>>> watchById(String id);
+}
